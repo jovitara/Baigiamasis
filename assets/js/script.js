7b45c10dd35d9a8724e94fec568e4a3b957618e9
jovitara <83599193+jovitara@users.noreply.github.com>
2021-06-18 20:20:06 +0300

    savarankiskas 06-18

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    // Priskiriame HTML elementui, slick functionaluma
+    $(".bit-slider-home").slick({
+        slidesToShow: 1,
+        dots: true,
+      });
+});
+$(document).ready(function () {
+    // Priskiriame HTML elementui, slick functionaluma
+    $(".bit-slider-profile").slick({
+        slidesToShow: 2,
+        dots: true,
+      });
+});
+$(document).ready(function () {
+    // Priskiriame HTML elementui, slick functionaluma
+    $(".bit-slider-contact").slick({
+        slidesToShow: 3,
+        dots: true,
+      });
+});
