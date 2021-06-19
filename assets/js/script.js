88ceb483a9441671615c74a1b297210b108bfa1d
jovitara <83599193+jovitara@users.noreply.github.com>
2021-06-19 13:13:05 +0300

    brand-logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,11 @@
-$('.responsive').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
+
+$(document).ready(function(){
+  
+$('.brand-logo-wraper').slick({
+  infinite: true,
   slidesToShow: 6,
   slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
 });
